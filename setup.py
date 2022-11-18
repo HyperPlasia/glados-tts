4ec1eb7544daf5fe299cab7a798f1369aed08a69
pleahmacaka <pleahmacaka@gmail.com>
2022-11-18 09:16:16 +0900

    :star:

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import os
 
 import pkg_resources
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="glados-tts",
-    version="1.0.1",
+    version="1.0.2",
     license="MIT",
     url='https://github.com/R2D2FISH/glados-tts',
     description="frok of https://github.com/R2D2FISH/glados-tts",
     author="https://github.com/R2D2FISH/glados-tts/graphs/contributors",
-    py_modules=["."],
-    packages=["."],
+    py_modules=["glados"],
+    packages=["glados"],
     requires=[
         str(r)
         for r in pkg_resources.parse_requirements(
